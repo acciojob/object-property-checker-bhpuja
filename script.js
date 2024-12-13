18780bd4023575950233bf1ccc3eaf9d8e200e6e
bhpuja <vpuja0421@gmail.com>
2024-12-13 15:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	// Global object definition
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF"
+};
+
+// Function to check if the object contains the given key
+function hasKey(key) {
+  return sampleObject.hasOwnProperty(key);
+}
+
+// Examples
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
+
 }
 
 // Do not change the code below
